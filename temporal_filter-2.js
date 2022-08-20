ba75df6e383f94853495140238329eb5f4d1b52f
Edimilson Rodrigues <edimilson.rodrigues.santos@usp.br>
2022-08-20 17:28:14 -0300

    Update temporal_filter-2.js

--- temporal_filter-2.js
+++ temporal_filter-2.js
@@ -23,14 +23,17 @@ var geometry =
 
 
 //Defines the input asset and delimits the export area
-var input_asset = 'users/edimilsonrodriguessantos/MAPBIOMAS/INFRAURBANA6-FT-v6'
+var input_asset = 'projects/mapbiomas-workspace/TRANSVERSAIS/INFRAURBANA7-FT/'
+var outputAsset = input_asset
+var version = '-FE2-gr1'
+var nextVersion = '-FE2-gr2'//'-FE1-gr2' 
+
 var ExportImage = function(image, geometry, year){
 
-    var imageName =  String(year)+ '-gr2-' + 1;
-    
+    var imageName =  String(year) + nextVersion;
     Export.image.toAsset({
         "image": image,
-        "assetId":"users/edimilsonrodriguessantos/MAPBIOMAS/INFRAURBANA6-FT-v6" + '/' + imageName,
+        "assetId": outputAsset + imageName,
         "description": imageName,
         "region": geometry,
         "scale": 30,
@@ -39,12 +42,9 @@ var ExportImage = function(image, geometry, year){
   };
 
 //Defines the list of years considered and separates the final years
-var years_mid = [1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,
-                 2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,
-                 2015,2016,2017
-                 ]
-var last_years = [2018, 2019]
-var last_year = [2020]
+var years_mid = ee.List.sequence(1985, 2018).getInfo()
+var last_years = [2019, 2020]
+var last_year = [2021]
 
 //Apply the filter for all years except the last three and export the results
 var filter_GR2 = years_mid.map(function(year){
@@ -52,12 +52,15 @@ var filter_GR2 = years_mid.map(function(year){
   var next2 = year + 2
   var next3 = year + 3
   
-  var year_0 = ee.Image(input_asset + '/' + year + '-gr1-' + 1)
-  var year_next = ee.Image(input_asset + '/' + next + '-gr1-' + 1)
-  var year_next2 = ee.Image(input_asset + '/' + next2 + '-gr1-' + 1)
-  var year_next3 = ee.Image(input_asset + '/' + next3 + '-gr1-' + 1)
+  var year_0 = ee.Image(input_asset + year + version)
+  var year_next = ee.Image(input_asset + next + version)
+  var year_next2 = ee.Image(input_asset + next2 + version)
+  var year_next3 = ee.Image(input_asset + next3 + version)
   
-  var cond = ee.ImageCollection([year_0,year_next,year_next2,year_next3]).sum().gte(2).multiply(year_0)
+  var cond = ee.ImageCollection([year_0,year_next,year_next2,year_next3])
+               .sum().gte(2).multiply(year_0)
+               .unmask()
+
   ExportImage(cond,geometry,year)
 })
 
@@ -69,12 +72,15 @@ var filter_GR2_lasts = last_years.map(function(year){
   var prev = year - 1
   var next = year + 1
   
-  var year_prev2 = ee.Image(input_asset + '/' + prev2 + '-gr1-' + 1)
-  var year_prev = ee.Image(input_asset +'/' + prev + '-gr1-' + 1)
-  var year_0 = ee.Image(input_asset + '/' + year + '-gr1-' + 1)
-  var year_next = ee.Image(input_asset + '/' + next + '-gr1-' + 1)
+  var year_prev2 = ee.Image(input_asset + prev2 + version)
+  var year_prev = ee.Image(input_asset + prev + version)
+  var year_0 = ee.Image(input_asset + year + version)
+  var year_next = ee.Image(input_asset + next + version)
   
-  var cond = ee.ImageCollection([year_prev2 ,year_prev, year_0,year_next]).sum().gte(2).multiply(year_0)
+  var cond = ee.ImageCollection([year_prev2 ,year_prev, year_0,year_next])
+               .sum().gte(2).multiply(year_0)
+               .unmask()
+
   ExportImage(cond,geometry,year)
 })
 
@@ -84,11 +90,14 @@ var filter_GR2_last = last_year.map(function(year){
   var prev2 = year - 2
   var prev = year - 1
   
-  var year_prev2 = ee.Image(input_asset + '/' + prev2 + '-gr1-' + 1)
-  var year_prev = ee.Image(input_asset +'/' + prev + '-gr1-' + 1)
-  var year_0 = ee.Image(input_asset + '/' + year + '-gr1-' + 1)
+  var year_prev2 = ee.Image(input_asset + prev2 + version)
+  var year_prev = ee.Image(input_asset + prev + version)
+  var year_0 = ee.Image(input_asset + year + version)
   
-  var cond = ee.ImageCollection([year_prev2 ,year_prev, year_0]).sum().gte(1)
+  var cond = ee.ImageCollection([year_prev2 ,year_prev, year_0])
+               .sum().gte(1).multiply(year_0)
+               .unmask()
+
   //Map.addLayer(cond,{bands: "classification", min: 0, max: 1, palette: ['black','red'], opacity: 0.40}, 'result2_' + year)
   ExportImage(cond,geometry,year)
 })
